246b4f1698b7614f20709e7e7104ba0a34ae9062
arakviel <insider.arakviel@gmail.com>
2024-10-29 19:02:31 +0200

    simple

---
+++ AdoNetAdapterDemoWpf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace AdoNetAdapterDemoWpf;
+
+public partial class MainWindow : Window
+{
+    private readonly UsersViewModel _viewModel;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _viewModel = new UsersViewModel();
+        DataContext = _viewModel;
+        Closing += MainWindow_Closing;
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        var newUser = new User { Name = "New User", Email = "user@example.com" };
+        _viewModel.AddUser(newUser);
+    }
+
+    private void EditButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (UsersGrid.SelectedItem is User selectedUser)
+        {
+            selectedUser.Name = "Updated Name"; // You may add edit dialog for user inputs
+            selectedUser.Email = "updated@example.com";
+            _viewModel.UpdateUser(selectedUser);
+        }
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (UsersGrid.SelectedItem is User selectedUser)
+        {
+            _viewModel.DeleteUser(selectedUser);
+        }
+    }
+
+    private void MainWindow_Closing(object sender, CancelEventArgs e)
+    {
+        _viewModel.SaveChanges();
+    }
+}
---
+++ AdoNetAdapterDemoWpf/UserViewModel.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+
+namespace AdoNetAdapterDemoWpf;
+
+public class UsersViewModel : INotifyPropertyChanged
+{
+    private readonly string _connectionString = "Server=.\\SQLEXPRESS;Database=Demo1;Integrated Security=True;TrustServerCertificate=True;";
+    private DataTable _usersTable;
+    private SqlDataAdapter _adapter;
+
+    public ObservableCollection<User> Users { get; set; }
+
+    public UsersViewModel()
+    {
+        Users = new ObservableCollection<User>();
+        LoadUsers();
+    }
+
+    public void LoadUsers()
+    {
+        _adapter = new SqlDataAdapter("SELECT * FROM Users", _connectionString);
+        new SqlCommandBuilder(_adapter);
+        _usersTable = new DataTable();
+        _adapter.Fill(_usersTable);
+
+        foreach (DataRow row in _usersTable.Rows)
+        {
+            Users.Add(new User
+            {
+                Id = (int)row["Id"],
+                Name = row["Name"].ToString(),
+                Email = row["Email"].ToString()
+            });
+        }
+    }
+
+    public void AddUser(User user)
+    {
+        var newRow = _usersTable.NewRow();
+        newRow["Name"] = user.Name;
+        newRow["Email"] = user.Email;
+        _usersTable.Rows.Add(newRow);
+
+        Users.Add(user);
+    }
+
+    public void UpdateUser(User user)
+    {
+        var row = _usersTable.Select($"Id = {user.Id}").FirstOrDefault();
+        if (row != null)
+        {
+            row["Name"] = user.Name;
+            row["Email"] = user.Email;
+        }
+    }
+
+    public void DeleteUser(User user)
+    {
+        var row = _usersTable.Select($"Id = {user.Id}").FirstOrDefault();
+        if (row != null)
+        {
+            row.Delete();
+            Users.Remove(user);
+        }
+    }
+
+    public void SaveChanges()
+    {
+        using var connection = new SqlConnection(_connectionString);
+        _adapter.Update(_usersTable);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string name) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
